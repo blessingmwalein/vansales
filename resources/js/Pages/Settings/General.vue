327c64ef61834970cb57779dab49a09e831dc890
Blessing Mwale <blessingm@equals.co.zw>
2023-10-06 10:43:50 +0200

    done

--- resources/js/Pages/Settings/General.vue
+++ resources/js/Pages/Settings/General.vue
@@ -83,7 +83,6 @@ export default {
                     }
                 });
         }
-
     },
 
     watch: {
@@ -159,7 +158,8 @@ export default {
 
                                 </div>
                                 <div class="mt-6" v-if="hasSettingChanged">
-                                    <button type="button" @click="updateSettings()" :class="{ 'opacity-25': isLoading }" :disabled="isLoading"
+                                    <button type="button" @click="updateSettings()" :class="{ 'opacity-25': isLoading }"
+                                        :disabled="isLoading"
                                         class="inline-flex items-center justify-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                         Update Changes
                                     </button>
